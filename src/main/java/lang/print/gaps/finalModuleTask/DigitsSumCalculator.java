85d777e4904e3acd5b06e71d339d4fc0a3d91c2e
davlatovv <zafargithub@gmail.com>
2023-01-15 01:00:14 +0500

    Final test

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void calculateSum(int number) {
+        int result = 0;
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
+        }
+        System.out.println(result);
     }
+    public static void main(String[] args){
+        calculateSum(2345);
+    }
+
 }
